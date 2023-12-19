968f75fcd31c8fe63f02f13cd746e91df9b7b86a
user <user@angger>
2023-12-19 14:09:31 +0700

    commit 1

---
+++ src/aplikasilogin/AplikasiLogin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasilogin;
+
+/**
+ *
+ * @author user
+ */
+public class AplikasiLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      loginView lv = new loginView();
+      lv.setLocationRelativeTo(null);
+      lv.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
